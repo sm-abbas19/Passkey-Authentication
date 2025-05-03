d3d4a456b452d4f69603cd79db4247486caa4826
Syed Muhammad Abbas <110503210+sm-abbas19@users.noreply.github.com>
2025-05-03 16:17:42 +0500

    Initial Commit 2

--- app/app.py
+++ app/app.py
@@ -1,9 +1,10 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_migrate import Migrate
 
 from models import db
+from auth.views import auth
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
@@ -11,11 +12,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-
 db.init_app(app)
 Migrate(app, db)
+
+app.register_blueprint(auth, url_prefix="/auth")
 
 
 @app.route("/")
 def index():
-    return "Hello, world!"
+    """The main homepage. This is a stub since it's a demo project."""
+    return render_template("index.html")
